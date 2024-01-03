16704254e69b2e37dc83564816ed7c8d558869c5
Dixit-Gumasana <dixitg3008@gmail.com>
2024-01-03 13:30:39 +0530

    Main.dart file update

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ToDo()
+      home: Splash()
     );
   }
 }
